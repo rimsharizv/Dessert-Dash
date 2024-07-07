61199024d2f136c052f9d303f84215c024909499
rimsharizv <rimsharizvi06@gmail.com>
2024-07-07 03:59:33 -0500

    Initial Commit

---
+++ DessertDash/DessertDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertDashApp.swift
+//  DessertDash
+//
+//  Created by Rimsha Rizvi on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
